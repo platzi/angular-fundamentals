27b3df5bcf66b62eeded36af44225f8f9a6f3194
Nicolas Molina Monroy <hi@nicobytes.com>
2021-07-20 18:51:28 -0400

    string interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-store';
+  name = 'Nicolas';
+  age = 18;
+  img = 'https://www.w3schools.com/howto/img_avatar.png';
 }
