ac34887820aa6765f38df59e752483b9e8b243f6
Nicolas Molina Monroy <hi@nicobytes.com>
2021-07-21 10:58:12 -0400

    :construction: event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,4 +15,12 @@ export class AppComponent {
     age: 18,
     avatar: 'https://www.w3schools.com/howto/img_avatar.png'
   }
+
+  toggleButton() {
+    this.btnDisabled = !this.btnDisabled;
+  }
+
+  increaseAge() {
+    this.person.age += 1;
+  }
 }
