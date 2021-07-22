34c87a9291fc832686b0d19157d545ef6731d4ec
Nicolas Molina Monroy <hi@nicobytes.com>
2021-07-22 10:54:13 -0400

    :construction: class & style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,11 @@ import { Product } from './product.model';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  widthImg = 10;
   name = 'Nicolas';
   age = 18;
   img = 'https://www.w3schools.com/howto/img_avatar.png';
   btnDisabled = true;
-
   person = {
     name: 'Nicolas',
     age: 18,
