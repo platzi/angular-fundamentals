f4ef58647b7978ea5ad6a7f28a0ea033be1fb3d1
Nicolas Molina Monroy <hi@nicobytes.com>
2021-07-22 11:34:36 -0400

    :construction: Form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,11 @@ export class AppComponent {
   age = 18;
   img = 'https://www.w3schools.com/howto/img_avatar.png';
   btnDisabled = true;
+  register = {
+    name: '',
+    email: '',
+    password: '',
+  }
   person = {
     name: 'Nicolas',
     age: 18,
@@ -84,5 +89,9 @@ export class AppComponent {
 
   deleteName(index: number) {
     this.names.splice(index, 1);
+  }
+
+  onRegister() {
+    console.log(this.register);
   }
 }
