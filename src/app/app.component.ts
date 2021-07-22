6a891e45e036adace54347f9823ea119fce0a233
Nicolas Molina Monroy <hi@nicobytes.com>
2021-07-22 11:11:02 -0400

    :lipstick: NgClass & NgStyles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,6 +20,11 @@ export class AppComponent {
   }
   names: string[] = ['Nico', 'Juli', 'Santi'];
   newName = '';
+  box = {
+    width: 100,
+    height: 100,
+    background: 'red'
+  };
   products: Product[] = [
     {
       name: 'EL mejor juguete',
